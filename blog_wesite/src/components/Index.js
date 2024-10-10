bf33f2d11e58a3a39d0665b98d64c942bcb405bf
santoshmethre22 <santoshmethre22@gmail.com>
2024-10-11 00:22:21 +0530

    blog project ---2

--- blog_wesite/src/components/Index.js
+++ blog_wesite/src/components/Index.js
@@ -1,8 +1,13 @@
 import Header from "./Header";
-
+import Container from "./container/Container";
 import Footer from "./Footer";
+import Logo from "./Logo";
+import LogoutBtn from "./Header/LogoutBtn";
 
 export{
     Header,
-    Footer
+    Footer,
+    Container,
+    Logo,
+    LogoutBtn
 }
